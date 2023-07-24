fee0141f338ff55ad870a455647e8af9fa52378a
SebasAgression <84996009+Tamefuzball7@users.noreply.github.com>
2023-07-23 23:40:02 -0500

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -167,6 +167,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'temp')
+
 TIKA_SERVER_ENDPOINT = 'http://localhost:9998/tika'
 
 TEMP_DIR = Path.joinpath(BASE_DIR, 'temp')
